8f56a86e7e103315ab732cea2bb3b623ccb5f2e9
Metehan Gultekin <mgmetehan@hotmail.com>
2022-12-05 17:31:33 +0300

    Add base project

---
+++ accountAndUser/src/main/java/com/mgmetehan/accountAndUser/AccountAndUserApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.accountAndUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAndUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountAndUserApplication.class, args);
+	}
+
+}
